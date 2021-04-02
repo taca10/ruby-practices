1517fc139bb235a0f1983e98a4cef254262b5e57
坪内崇人 <taka.ur.0505@gmail.com>
2021-04-02 17:18:50 +0900

    fizzbuzzのプログラム作成

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizz_buzz(number)
+  if number % 15 == 0
+    output = "FizzBuzz"
+  elsif number % 3 == 0
+    output = "Fizz"
+  elsif number % 5 == 0
+    output = "Buzz"
+  else
+    output = number
+  end
+  puts output
+end
+(1..20).each{|n| fizz_buzz(n) }
